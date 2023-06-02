9ace603919cbe1abe1ec4a3587fb5a9c2bd04324
Griffen Wakelin <gr246427@dal.ca>
2023-06-02 13:49:22 -0300

    updated answers

--- 2_FunctionsLibraries/2_14_monteCarloPI/monteCarloPI.cpp
+++ 2_FunctionsLibraries/2_14_monteCarloPI/monteCarloPI.cpp
@@ -1,36 +1,31 @@
-// literally cannot get this to work correctly.....
-
+#include <cmath>
 #include <iostream>
-#include <math.h>
+using namespace std;
 
-double randomReal(double low, double high);
+const unsigned N_TRIALS = 100000000;
+
+bool dartLandedInCircle();
+double estimatePI(unsigned nTrials);
 
 int main(void) {
-    int n = 1000000;
-    double x, y;
-    double num = 0.0, denom = 0.0;
-
-    for (int i = 0; i < n; ++i) {
-        // x = ((rand() / (double(RAND_MAX) + 1)) * 2) - 1;
-        // y = ((rand() / (double(RAND_MAX) + 1)) * 2) - 1;
-
-        x = randomReal(-1, 1);
-        y = randomReal(-1, 1);
-
-        if (((x * x) + (y * y)) < 1) {
-            num++;
-        } else {
-            denom++;
-        }
-    }
-
-    double estim = (num / denom);
-    std::printf("%f\n", estim);
-    return 0;
+    cout << "After " << N_TRIALS << " trials, PI estimation equals "
+         << estimatePI(N_TRIALS) << endl;
 }
 
-double randomReal(double low, double high) {
-    double d = rand() / (double(RAND_MAX) + 1);
-    double s = d * (high - low);
-    return low + s;
+double estimatePI(unsigned nTrials) {
+    int landedInCircle = 0;
+
+    for (int throws = 0; throws < N_TRIALS; throws++) {
+        if (dartLandedInCircle())
+            landedInCircle++;
+    }
+
+    return (landedInCircle / double(nTrials)) * 4.0;
+}
+
+bool dartLandedInCircle() {
+    double x = ((rand() / (double(RAND_MAX) + 1)) * 2) - 1;
+    double y = ((rand() / (double(RAND_MAX) + 1)) * 2) - 1;
+
+    return (x * x + y * y < 1);
 }
